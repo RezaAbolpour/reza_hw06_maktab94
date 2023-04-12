0fb866b704b986afa76b17f956480ea24cd892e7
Reza <rezamis99vip@gmail.com>
2023-04-12 18:04:35 +0430

    laptop store

---
+++ hw8-7/index.js
@@ -0,0 +1,27 @@
+const listVideo=["1.mp4","2.mp4","3.mp4","4.mp4"]
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    console.log(tr)
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+function getRaw(row){
+    video = document.getElementById("videoPlayer");
+    const index = row.rowIndex-1;
+    video.src="video/"+listVideo[index];
+    video.play();
+}
